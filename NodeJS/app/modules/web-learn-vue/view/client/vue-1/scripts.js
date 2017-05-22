900c03425f70b8727a6f0fda6441074867963bb3
Xuan Toan <xuantoancth541944@gmail.com.vn>
2017-05-22 09:41:30 +0700

    remove old project

--- NodeJS/app/modules/web-learn-vue/view/client/vue-1/scripts.js
+++
@@ -1,8 +0,0 @@
-Vue.config.debug = true;
-Vue.config.devtools = true;
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue Testttttt!'
-    },
-})
--- NodeJS/gulp/serve.js
+++
@@ -1,3 +0,0 @@
-var gulp = require('gulp');
-gulp.task('admin-serve',['admin-injectJS', 'admin-injectCSS']);
-gulp.task('site-serve',['site-injectJS', 'site-injectCSS']);
