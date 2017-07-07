540b398dd1fd45eb9b4412f5a0b9a9b02013d04d
Xuan Toan <xuantoancth541944@gmail.com.vn>
2017-07-07 15:18:45 +0700

    update learn vue

---
+++ vue-loader/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <message>Hello world</message>
+    <message>Hello world saf as</message>
+    <message>Hello worldf as f</message>
+    <message>Hello worlda sf</message>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue'
+import Counter from './components/Counter.vue'
+export default {
+  name: 'app',
+  components:{ Message , Counter},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
