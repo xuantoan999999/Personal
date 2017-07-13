d48bf280418b5f0514032cca2d59effca8eba997
Xuan Toan <xuantoancth541944@gmail.com.vn>
2017-07-13 10:54:20 +0700

    remove mongo

--- adonis/bootstrap/app.js
+++ adonis/bootstrap/app.js
@@ -24,12 +24,9 @@ const providers = [
     'adonis-framework/providers/SessionProvider',
     'adonis-framework/providers/StaticProvider',
     'adonis-framework/providers/ViewProvider',
-    // 'adonis-lucid/providers/DatabaseProvider',
-    // 'adonis-lucid/providers/LucidProvider',
-    // 'adonis-lucid/providers/FactoryProvider',
-    'adonis-lucid-mongodb/providers/DatabaseProvider',
-    'adonis-lucid-mongodb/providers/LucidMongoProvider',
-    'adonis-lucid-mongodb/providers/FactoryProvider',
+    'adonis-lucid/providers/DatabaseProvider',
+    'adonis-lucid/providers/LucidProvider',
+    'adonis-lucid/providers/FactoryProvider',
     'adonis-middleware/providers/AppMiddlewareProvider',
     'adonis-auth/providers/AuthManagerProvider',
     'adonis-websocket/providers/WsProvider'
@@ -45,14 +42,10 @@ const providers = [
 |
 */
 const aceProviders = [
-    // 'adonis-lucid/providers/CommandsProvider',
-    // 'adonis-lucid/providers/MigrationsProvider',
-    // 'adonis-lucid/providers/SchemaProvider',
-    // 'adonis-lucid/providers/SeederProvider',
-    'adonis-lucid-mongodb/providers/CommandsProvider',
-    'adonis-lucid-mongodb/providers/MigrationsProvider',
-    'adonis-lucid-mongodb/providers/SchemaProvider',
-    'adonis-lucid-mongodb/providers/SeederProvider',
+    'adonis-lucid/providers/CommandsProvider',
+    'adonis-lucid/providers/MigrationsProvider',
+    'adonis-lucid/providers/SchemaProvider',
+    'adonis-lucid/providers/SeederProvider',
     'adonis-ace/providers/CommandProvider',
     'adonis-commands/providers/GeneratorsProvider',
     'adonis-commands/providers/HelperCommandsProvider',
@@ -72,24 +65,18 @@ const aceProviders = [
 const aliases = {
     Command: 'Adonis/Src/Command',
     Config: 'Adonis/Src/Config',
-    // Database: 'Adonis/Src/Database',
+    Database: 'Adonis/Src/Database',
     Env: 'Adonis/Src/Env',
     Event: 'Adonis/Src/Event',
-    // Factory: 'Adonis/Src/Factory',
+    Factory: 'Adonis/Src/Factory',
     Hash: 'Adonis/Src/Hash',
     Helpers: 'Adonis/Src/Helpers',
     Lucid: 'Adonis/Src/Lucid',
     Middleware: 'Adonis/Src/Middleware',
     Route: 'Adonis/Src/Route',
-    // Schema: 'Adonis/Src/Schema',
+    Schema: 'Adonis/Src/Schema',
     View: 'Adonis/Src/View',
     Ws: 'Adonis/Addons/Ws',
-
-    Database: 'Adonis/Src/Database',
-    Lucid: 'Adonis/Src/LucidMongo',
-    Schema: 'Adonis/Src/Schema',
-    Migrations: 'Adonis/Src/Migrations',
-    Factory: 'Adonis/Src/Factory'
 }
 
 /*
