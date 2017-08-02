e22ef20e375e130e6f3ca68af029b92620e24f85
Xuan Toan <xuantoancth541944@gmail.com.vn>
2017-08-02 08:19:58 +0700

    update angular 4

--- angular4/src/app/app.component.ts
+++ angular4/src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    template: `
-    <h1>dng ksdng</h1>
-    <div *ngIf="myArr; then tmpl1 else tmpl2"></div>
-
-    <ng-template #tmpl1>True</ng-template>
-    <ng-template #tmpl2>False</ng-template>
-  `,
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-    // myArr = ['him', 'hers', 'yours'];
-    myArr = true;
+  title = 'app';
 }
