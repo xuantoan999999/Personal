1b3188ab77b1aa1e7a1828ecb4ad64010c5f43c6
Xuan Toan <xuantoancth541944@gmail.com.vn>
2017-09-15 07:58:31 +0700

    update

--- adonis/resources/admin/src/app/app.module.ts
+++ adonis/resources/admin/src/app/app.module.ts
@@ -1,14 +1,39 @@
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule, MdInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'dang-nhap',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dang-nhap',
+        component: LoginComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
