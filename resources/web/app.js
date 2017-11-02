8099ae416a31f381e7bcce10ce408898b76e91a8
Xuan Toan <xuantoancth541994@gmail.com>
2017-11-02 17:18:49 +0700

    update

--- resources/web/app.js
+++ resources/web/app.js
@@ -4,4 +4,4 @@ import './style/main.scss';
 // Script file module
 import './script/module/home/home';
 import './script/module/nav';
-// import './script/module/learn-1';
+import './script/module/learn-1';
